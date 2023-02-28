1c0fbd6753effcc62a3bd8c521b410f92963844b
yaofei zheng <yaofei.zheng@dell.com>
2023-02-28 10:56:45 -0800

    make dump json

--- pcidump2cvs.c
+++ pcidump2cvs.c
@@ -13,41 +13,30 @@ int main()
     pci_init(pacc);
     pci_scan_bus(pacc);
 
-    printf("DBDF, Vendor ID, Device ID, Class ID, Base Address 0, Size 0, Base Address 1, Size 1, Base Address 2, Size 2, Vendor, Device, Class\n");
+    printf("[\n");
     for (dev = pacc->devices; dev; dev = dev->next)
     {
-        pci_fill_info(dev, PCI_FILL_IDENT | PCI_FILL_BASES | PCI_FILL_CLASS |PCI_FILL_PHYS_SLOT);
+        pci_fill_info(dev, PCI_FILL_IDENT | PCI_FILL_BASES | PCI_FILL_CLASS | PCI_FILL_PHYS_SLOT);
 
-        printf(
-            "%04x:%02x:%02x.%d,"    // DBDF
-            "0x%04X,"               // Vendor ID
-            "0x%04X,"               // Device ID
-            "0x%04X,"               // Class ID, 
-            "0x%016X,"              // Base Address 0, 
-            "0x%08X,"               // Size 0, 
-            "0x%016X,"              // Base Address 1, 
-            "0x%08X,"               // Size 1, 
-            "0x%016X,"              // Base Address 2, 
-            "0x%08X,",              // Size 2, 
-            dev->domain, dev->bus, dev->dev, dev->func,
-            dev->vendor_id,
-            dev->device_id,
-            dev->device_class,
-            dev->base_addr[0], dev->size[0],
-            dev->base_addr[1], dev->size[1],
-            dev->base_addr[2], dev->size[2]);
+        printf("{");
+        printf("\"DBDF\":\"%04x:%02x:%02x.%d\",", dev->domain, dev->bus, dev->dev, dev->func);
+        printf("\"Vendor ID\":\"0x%04X\",", dev->vendor_id);
+        printf("\"Device ID\":\"0x%04X\",", dev->device_id);
+        printf("\"Class ID\":\"0x%04X\",", dev->device_class);
+        printf("\"Base Address 0\":\"0x%016X\",\"Size 0\":\"0x%08X\",", dev->base_addr[0], dev->size[0]);
+        printf("\"Base Address 1\":\"0x%016X\",\"Size 1\":\"0x%08X\",", dev->base_addr[1], dev->size[1]);
+        printf("\"Base Address 2\":\"0x%016X\",\"Size 2\":\"0x%08X\",", dev->base_addr[2], dev->size[2]);
 
-        // Vendor, 
-        name= pci_lookup_name(pacc, namebuf, sizeof(namebuf), PCI_LOOKUP_VENDOR, dev->vendor_id, dev->device_id);
-        printf("%s,", name);
-        // Device, 
+        name = pci_lookup_name(pacc, namebuf, sizeof(namebuf), PCI_LOOKUP_VENDOR, dev->vendor_id, dev->device_id);
+        printf("\"Vendor\":\"%s\",", name);
         name = pci_lookup_name(pacc, namebuf, sizeof(namebuf), PCI_LOOKUP_DEVICE, dev->vendor_id, dev->device_id);
-        printf("%s,", name);
-        // Class
-        name=pci_lookup_name(pacc, namebuf, sizeof(namebuf), PCI_LOOKUP_CLASS, dev->device_class);
-        printf("%s", name);
-        printf("\n", name);
+        printf("\"Device\":\"%s\",", name);
+        name = pci_lookup_name(pacc, namebuf, sizeof(namebuf), PCI_LOOKUP_CLASS, dev->device_class);
+        printf("\"Class\":\"%s\"", name);
+
+        printf("},\n");
     }
-    pci_cleanup(pacc);		/* Close everything */
+    printf("]\n");
+    pci_cleanup(pacc);
     return 0;
 }
